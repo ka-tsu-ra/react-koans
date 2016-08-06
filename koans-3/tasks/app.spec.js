9d65a9743bb361f5998e5fa906cd90368d1dd7f2
katsuraku <kirsten.jones@ft.com>
2016-08-06 21:05:54 +0100

    Sinon stuff

--- koans-3/tasks/app.spec.js
+++ koans-3/tasks/app.spec.js
@@ -6,8 +6,9 @@ import * as api from "../app/api/index";
 import sinon from 'sinon';
 
 describe('Nav component', () => {
-    it('should navitate to / when clicking the home page link', () => {
-      const router = { push: sinon.spy() };
+    it('should navigate to / when clicking the home page link', () => {
+      const fakeRouter = { push: sinon.spy() };
+      const jsn = {context: {router: fakeRouter}};
       // Hint:
       // In the app/components/Nav the router is injected using the Nav.contextTypes = {
       //    router: React.PropTypes.object.isRequired,
@@ -17,32 +18,37 @@ describe('Nav component', () => {
       // To fix it you  must inject the const router = { push: sinon.spy() }; we just created into the mount(<Nav />) above
       // Nore hints? check the https://github.com/airbnb/enzyme/blob/master/docs/api/shallow.md
       const wrapper = shallow(
-        <Nav />
+        <Nav />, jsn
       );
       wrapper.find('.home').simulate('click');
+      sinon.assert.calledWith(fakeRouter.push, '/');
     })
 
-    it('should navitate to /todos when clicking the todos link', () => {
-      const router = { push: sinon.spy() };
+    it('should navigate to /todos when clicking the todos link', () => {
+      const fakeRouter = { push: sinon.spy() };
+      const jsn = {context: {router: fakeRouter}};
+
       // Hint, you need to do 2 things:
-      // 1. Inject the context like you did in the previous exercise 
+      // 1. Inject the context like you did in the previous exercise
       // 2. Shallow does not "mount"" the component and so it does not execute lifecycle methods, it doesn't create refs,...
-      const wrapper = shallow(
-        <Nav />
+      const wrapper = mount(
+        <Nav />, jsn
       );
       // don't change next line, you must use .ref() instead of .find()
       wrapper.ref('todos').simulate('click');
-      sinon.assert.calledWith(router.push, '/todos');
+      sinon.assert.calledWith(fakeRouter.push, '/todos');
     })
 });
 
 describe('TodoList component', () => {
     it('should fetch the todos of the current user', () => {
-      // api/index.js is not injected into the TodoList, instead it's imported using the import syntax. 
+      // api/index.js is not injected into the TodoList, instead it's imported using the import syntax.
       // So how do you "fake" it?
       // Hint: Use sinon stubs
+
+      sinon.stub(api, 'getTodos').returns(new Promise(function () {}));
       const wrapper = mount(
-        <TodoList />
+        <TodoList userId='123'/>
       );
       sinon.assert.calledWith(api.getTodos, '123');
     })
